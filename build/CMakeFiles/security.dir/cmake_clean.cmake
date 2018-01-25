a1c594f364db99cafdbf28b9709c8d6f25c55b29
Paul.Lin <paul415236@gmail.com>
2018-01-25 17:53:32 +0800

    add que

---
+++ build/CMakeFiles/security.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/security.dir/src/main.cpp.o"
+  "CMakeFiles/security.dir/src/que.cpp.o"
+  "security.pdb"
+  "security"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/security.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
